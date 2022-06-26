5cbb32692fbc88992d03d9135efe47d92e47c8d6
jiut <1151386302@qq.com>
2022-06-26 14:04:09 +0800

    player complete

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ def music():
     for i in all_files:
         print(i)
         music_list.append(i)
+    # music_list = Markup(music_list) Markup返回的是字符串，这会导致页面中{{ music_list[0] }}异常，所以应该在页面中用safe或tojson标记
     return render_template('music.html', music_list=music_list)
 
 
