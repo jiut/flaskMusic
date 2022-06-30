17c0780cc5018e21916629de8e987b07d6aa2699
jiut <1151386302@qq.com>
2022-06-30 15:14:17 +0800

    播放器完善

--- app.py
+++ app.py
@@ -3,11 +3,13 @@
 
 import requests
 from flask import Flask, render_template, url_for, redirect, flash, request
+import urllib.request
 
 app = Flask(__name__)
 app.secret_key = 'dev'
 
 url = "http://localhost:3000"
+
 
 @app.route('/')
 def music():
@@ -26,56 +28,84 @@ def login():
             flash('input.' + text)
             return redirect(url_for('login'))
 
-
     return render_template("login.html", title="登录")
+
 
 @app.route('/login/<int:phone>')
 def phoneLogin(phone):
     requests.get(url + "/captcha/sent?phone=" + str(phone))
     return None
 
+
 @app.route('/test', methods=['GET'])
 def test():
-    music_list = [{"name":"请发起搜索", "link":"/"}]
+    music_list = [{"name": "请发起搜索", "link": "/"}]
     return render_template("music_test.html", music_list=music_list, title="测试")
 
 
 @app.route('/musictest', methods=["GET"])
 def musicSearch():
-    f1 = open('./test1.txt', 'a', encoding="utf-8")
-    f1.truncate(0)
-    f2 = open('./test2.txt', 'a', encoding="utf-8")
-    f2.truncate(0)
+    f_search = open('./SearchCache.txt', 'r+', encoding="utf-8")
+    search_cache = eval(f_search.readline())
+    f_value = open('./ValueCache.txt', 'r+', encoding="utf-8")
+    value_cache = eval(f_value.readline())
+
+    is_value_change = 0
 
     key = request.args.get('key')
-    search = requests.get(url + "/cloudsearch?keywords=" + key).json()
-    f1.write(str(search))
-    f1.close()
 
-    text = search['result']['songs']
-    music_list = {"result":[]}
-    target = {"result":[]}
-    for song in text:
+    if key in search_cache:
+        search = search_cache[key]
+    else:
+        search = requests.get(url + "/cloudsearch?keywords=" + key).json()
+        search_cache[key] = search
+        f_search.truncate(0)
+        f_search.write(str(search_cache))
+
+    f_search.close()
+
+    songs = search['result']['songs']
+    music_list = {"result": []}
+    for song in songs:
         name = song["name"]
         id = song["id"]
-        origin = requests.get(url + "/song/url?id=" + str(id)).json()
-        link = origin['data'][0]['url']
-        dict = {"name": name, "link": link}
+        if (id in value_cache) and ("link" in value_cache[id]) and (
+                urllib.request.urlopen(value_cache[id]["link"]).code != 206):
+            link = value_cache[id]["link"]
+        else:
+            link = requests.get(url + "/song/url?id=" + str(id)).json()
+            link = link['data'][0]['url']
+            value_cache[id] = {"link": link}
+            is_value_change = is_value_change + 1
+
+        if (id in value_cache) and ("lrc" in value_cache[id]):
+            lrc = value_cache[id]["lrc"]
+        else:
+            lrc = requests.get(url + "/lyric?id=" + str(id)).json()
+            lrc = lrc["lrc"]["lyric"]
+            value_cache[id].update({"lrc": lrc})
+            is_value_change = is_value_change + 1
+
+        is_value_change = False
 
         ar = song["ar"]
         al = song["al"]
-        alia = []
-        if(song["alia"]):
-            alia = song["alia"]
-        dict2 = {"name":name, "id":id, "ar":ar, "al":al, "alia":alia}
-        # print(dict)
-        target["result"].append(dict2)
+
+        artist_list = ''
+        for artist in ar:
+            artist_list = artist_list + artist['name'] + '/'
+
+        artist_list = artist_list[:-1]
+        dict = {"name": name, "link": link, "ar": artist_list, "lrc": lrc, "alPic": al['picUrl'] + "?param=130y130"}
 
         music_list["result"].append(dict)
-    flash(str(target))
-    f2.write(str(target))
-    f2.close()
+
+    if is_value_change > 0:
+        f_value.truncate(0)
+        f_value.write(str(value_cache))
+    f_value.close()
     return music_list
+
 
 if __name__ == '__main__':
     app.run()
